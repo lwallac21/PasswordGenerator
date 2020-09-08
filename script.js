9c3a4957bb7344747a3bf3aa79549f8c53a3b0a8
Lawrence Wallace <lwallac21@gmail.com>
2020-09-08 11:47:17 -0400

    So, I have got all the objects and arrays and functions in order; working on the final for loop.

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 //DOM elements
-var passwordEl = document.getElementById(#password)
-var generateEl = document.getElementById(#generate)
+var passwordEl = document.getElementById("password")
+var generateEl = document.getElementById("generate")
 
 // Strings for getting random letters and numbers
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerCase = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "0123456789"
 var symbols = "~!@#$%^&*()_+=-`"
+
+
+        // Random genrator functions
+        function getLower() {
+            return lowerCase.charAt(Math.floor(Math.random()*lowerCase.length));
+        }
+        
+        function getUpper() {
+            return upperCase.charAt(Math.floor(Math.random()*upperCase.length));
+        }
+        
+        function getNumbers() {
+            return numbers.charAt(Math.floor(Math.random()*numbers.length));
+        }
+        
+        function getSymbols() {
+            return symbols.charAt(Math.floor(Math.random()*symbols.length));
+        }
 
 // Prompts and Confirms for password parameters
 document.querySelector('#generate').addEventListener('click', function() {
@@ -26,55 +44,64 @@ document.querySelector('#generate').addEventListener('click', function() {
     }
     
     var passUp = confirm ("Would you like upper case letters in your password?");
-        if (passUp == true) {
-            return true
-        }
+
     var passLow = confirm ("Would you like lower case letters in your password?");
-        if (passLow == true) {
-            return true
-        }
+
     var passNum = confirm ("Would you like numbers in your password?");
-        if (passNum == true) {
-            return true
-        }
-    var PassSym = confirm ("Would you like special characters in your password?");
-        if (passSym == true) {
-            return true
-        }
-    var hasLength = passLength.value;
-    var hasUpper = passUp.value;
-    var hasLower = passLow.value;
-    var hasNum = passNum.value;
-    var hasSym = passSym.value;
-    passwordEL.innertext = generatePassword(hasLength, hasUpper, hasLower, hasNum, hasSym)
+
+    var passSym = confirm ("Would you like special characters in your password?");
+
+    console.log(passUp, passLow, passNum, passSym)
+
+var hasLower = passLow;
+    hasUpper = passUp;
+    hasNumber = passNum;
+    hasSymbol = passSym;
+
+    generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, passLength)
+
+function generatePassword(lower, upper, number, symbol, passLength){
+
+
+    var newPassword = "";
+    var typesCount = lower + upper + number + symbol;
+
+    console.log(typesCount);
+
+     var typesArray = [{lower}, {upper}, {number}, {symbol}].filter (
+        item => Object.values(item)[0]    
+    );
+
+    console.log("Array = ", typesArray);
+
+    for (var i = 0; i < passLength; i+=typesCount) {
+        typesArray.forEach(type => {
+            var functionName = Object.keys(type)[0];
+            console.log(functionName)
+            newPassword += randomFunctions[functionName]();
+        });
+
+
+        
+        console.log (password)
+    }
+}
+console.log(password)
+//     passwordEL.innertext = generatePassword(hasLength, hasUpper, hasLower, hasNum, hasSym);
+// // generate password function
+// function generatePassword() {
+//     var generatedPassword = ''
+//     var typesCount = hasLower + hasUpper + hasNum + hasSym;
+//     console.log(typesCount);
+// }
+// console.log(generatePassword())
 });
-
-// generate password function
-
-
-
 var randomFunctions = {
     upper: getUpper,
     lower: getLower,
     nums: getNumbers,
-    syms: getSymbols,
-};
-
-// Random genrator functions
-function getLower() {
-    return lowerCase.charAt(Math.floor(Math.random()*lowerCase.length));
+    syms: getSymbols
 }
 
-function getUpper() {
-    return upperCase.charAt(Math.floor(Math.random()*upperCase.length));
-}
 
-function getNumbers() {
-    return numbers.charAt(Math.floor(Math.random()*numbers.length));
-}
 
-function getSymbols() {
-    return symbols.charAt(Math.floor(Math.random()*symbols.length));
-}
-
-console.log (getSymbols(), getUpper(), getLower(), getNumbers())
